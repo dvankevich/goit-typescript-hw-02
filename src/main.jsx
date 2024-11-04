2a05925f7b5051a893c9abbf3872bb6434f26908
dvankevich <dvankevich@gmail.com>
2024-11-04 18:19:19 +0200

    add react-hot-toast library

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import 'the-new-css-reset/css/reset.css';
+import 'modern-normalize';
 import './index.css';
 import App from './components/App';
 
