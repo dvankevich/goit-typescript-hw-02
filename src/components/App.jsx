d69dc553b289aca8c62ac882f9afe8abf63ad5c2
dvankevich <dvankevich@gmail.com>
2024-11-04 17:44:48 +0200

    create directory structure

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import SearchBar from './SearchBar/SearchBar';
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+      <h1>goit-react-hw-04</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+const SearchBar = () => {
+  return (
+    <header>
+      <form>
+        <input type="text" placeholder="Search images and photos" />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
+};
+
+export default SearchBar;
