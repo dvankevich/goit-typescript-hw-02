a33ed091d99fdf137d7cd87329229f900619eac6
dvankevich <dvankevich@gmail.com>
2024-11-04 18:07:01 +0200

    add SearchBar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import SearchBar from './SearchBar/SearchBar';
 
+const handleSearch = searchTerm => {
+  console.log(searchTerm);
+};
+
 function App() {
   return (
     <>
-      <SearchBar />
+      <SearchBar onSubmit={handleSearch} />
       <h1>goit-react-hw-04</h1>
     </>
   );
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,18 @@
-const SearchBar = () => {
+const SearchBar = ({ onSubmit }) => {
+  const handleSubmit = evt => {
+    const form = evt.target;
+    evt.preventDefault();
+    onSubmit(form.elements.searchTerm.value);
+    form.reset();
+  };
   return (
     <header>
-      <form>
-        <input type="text" placeholder="Search images and photos" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchTerm"
+          placeholder="Search images and photos"
+        />
         <button type="submit">Search</button>
       </form>
     </header>
