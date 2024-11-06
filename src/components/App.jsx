2c2c974f5cc229cd39984d83ed8752085a364252
dvankevich <dvankevich@gmail.com>
2024-11-06 17:57:30 +0200

    create handleSearch function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import ErrorMessage from './ErrorMessage/ErrorMessage';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import SearchBar from './SearchBar/SearchBar';
-import axios from 'axios';
+import { fetchResponse } from '../unsplash-api';
 
 function App() {
   const [response, setResponse] = useState({});
@@ -13,45 +13,20 @@ function App() {
 
   const handleSearch = searchTerm => {
     console.log(searchTerm);
+    async function getImages() {
+      try {
+        setLoading(true);
+        const apiResponse = await fetchResponse();
+        setResponse(apiResponse);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getImages();
   };
 
-  // useEffect(() => {
-  //   async function fetchResponse() {
-  //     const headersList = {
-  //       Accept: '*/*',
-  //       'Accept-Version': 'v1',
-  //       Authorization: 'Client-ID o4C13rGbT_u5Vq3h83n03CpOGqwS0N_oj1Gd8F1ik0g',
-  //     };
-  //     const reqOptions = {
-  //       url: 'https://api.unsplash.com/search/photos?query=grogu&per_page=3&page=1',
-  //       method: 'GET',
-  //       headers: headersList,
-  //     };
-
-  //     try {
-  //       setLoading(true);
-  //       const response = await axios.request(reqOptions);
-  //       setResponse(response);
-  //     } catch (error) {
-  //       // Встановлюємо стан error в true
-  //       setError(true);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   }
-
-  //   fetchResponse();
-  // }, []);
-
-  // const ResponsePrint = ({ response }) => {
-  //   const { data, headers } = response;
-  //   const { total_pages, results } = data;
-  //   //const { link } = headers;
-  //   console.log(total_pages, results, headers);
-  //   //console.log(response.headers['link']);
-
-  //   return <h2>response print</h2>;
-  // };
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
---
+++ src/unsplash-api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// export const fetchArticlesWithTopic = async topic => {
+//   const response = await axios.get(`/search?query=${topic}`);
+//   return response.data.hits;
+// };
+
+export async function fetchResponse() {
+  const headersList = {
+    Accept: '*/*',
+    'Accept-Version': 'v1',
+    Authorization: 'Client-ID o4C13rGbT_u5Vq3h83n03CpOGqwS0N_oj1Gd8F1ik0g',
+  };
+  const reqOptions = {
+    url: 'https://api.unsplash.com/search/photos?query=grogu&per_page=3&page=1',
+    method: 'GET',
+    headers: headersList,
+  };
+
+  const response = await axios.request(reqOptions);
+  return response;
+}
