8e5377a7e413ab419ccba3e9e49eeabeeee71f61
dvankevich <dvankevich@gmail.com>
2024-11-07 19:43:35 +0200

    axios error handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,11 +43,29 @@ function App() {
         const apiResponse = await fetchResponse(
           `${apiUrl}?query=${searchTerm}&per_page=${perPage}&page=1`
         );
-        setResults(apiResponse.data.results);
+        setResults(prevResults => {
+          [...prevResults, ...apiResponse.data.results];
+        });
         setNextUrl(getNextUrl(apiResponse.headers.link));
       } catch (error) {
+        // https://rapidapi.com/guides/handle-axios-errors
         setError(true);
-        console.log(error.message);
+        if (error.response) {
+          // Request made but the server responded with an error
+          console.log('Request made but the server responded with an error');
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // Request made but no response is received from the server.
+          console.log(
+            'Request made but no response is received from the server.'
+          );
+          console.log(error.request);
+        } else {
+          // Error occured while setting up the request
+          console.log('Error:', error.message);
+        }
       } finally {
         setLoading(false);
       }
@@ -56,7 +74,7 @@ function App() {
   };
 
   useEffect(() => {
-    console.log(nextUrl);
+    console.log('nextUrl: ', nextUrl);
   }, [nextUrl]);
 
   useEffect(() => {
