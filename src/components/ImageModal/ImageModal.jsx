540edf81acc6e76ced1e44308ced65198b295bdc
dvankevich <dvankevich@gmail.com>
2024-11-08 20:45:38 +0200

    add image info

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -11,12 +11,22 @@ const ImageModal = ({ isOpen, closeModal, img }) => {
       className={s.Modal}
       overlayClassName={s.Overlay}
     >
+      <h2>{img.alt_description}</h2>
       <div className={s.image}>
         <img src={img.urls.regular} alt={img.alt_description} />
       </div>
-      <h2>{img.alt_description}</h2>
-
       <p>Likes: {img.likes}</p>
+      <p>{img.description}</p>
+
+      <h3>Author</h3>
+      <p>Name: {img.user.name}</p>
+      <p>location: {img.user.location}</p>
+      {img.user.portfolio_url && (
+        <a href={img.user.portfolio_url} target="_blank">
+          Portfolio
+        </a>
+      )}
+      <p>----------</p>
       <button onClick={closeModal}>close</button>
     </Modal>
   );
