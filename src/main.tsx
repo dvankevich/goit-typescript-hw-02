f27d7c4bbc5115bb6775bef5f3ed6988456e478e
dvankevich <dvankevich@gmail.com>
2024-12-22 20:24:12 +0200

    create ts config files

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import 'modern-normalize';
 import './index.css';
 import App from './components/App';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
