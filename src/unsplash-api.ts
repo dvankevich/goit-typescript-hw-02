edc379c4f913317f9388cc4af4da1e7f415ce46f
dvankevich <dvankevich@gmail.com>
2024-12-23 19:15:56 +0200

    add types for axios

--- src/unsplash-api.ts
+++ src/unsplash-api.ts
@@ -1,28 +1,29 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
+import { AxiosResponse } from 'axios';
 
-const apiUrl = 'https://api.unsplash.com/search/photos';
-const perPage = 15;
+const apiUrl: string = 'https://api.unsplash.com/search/photos';
+const perPage: number = 15;
 
 // захист так собі, але хоч в репозиторії світитись не буде
 // цей ключ видно в request headers
-const apiKey = import.meta.env.VITE_API_KEY;
+const apiKey: string = import.meta.env.VITE_API_KEY;
 //console.log(apiKey);
 
-
- 
-export async function fetchData(query, page) {
-    const headersList = {
+export async function fetchData(
+  query: string,
+  page: number
+): Promise<AxiosResponse<any>> {
+  const headersList = {
     Accept: '*/*',
     'Accept-Version': 'v1',
     Authorization: `Client-ID ${apiKey}`,
-    };
-    const reqOptions = {
+  };
+  const reqOptions: AxiosRequestConfig = {
     url: `${apiUrl}?query=${query}&per_page=${perPage}&page=${page}`,
     method: 'GET',
-    headers: headersList,
-    };
-  
+    headers: headersList as RawAxiosRequestHeaders,
+  };
+
   const response = await axios.request(reqOptions);
   return response;
-  
 }
