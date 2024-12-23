af20f8e77e2c648500c7d2e08efa9318b4bbbe01
dvankevich <dvankevich@gmail.com>
2024-12-23 21:25:17 +0200

    add styles to SearchBar.tsx

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,19 @@
 import toast, { Toaster } from 'react-hot-toast';
 import s from './SearchBar.module.css';
 
-const SearchBar = ({ onSubmit }) => {
+interface SearchBarProps {
+  onSubmit: (searchTerm: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
   const showToast = () =>
     toast.error('search text must be longer than 2 characters');
-  const handleSubmit = evt => {
-    const form = evt.target;
-    const searchTerm = form.elements.searchTerm.value.trim();
+  const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    const form = evt.target as HTMLFormElement;
+    //const searchTerm = form.elements.searchTerm.value.trim();
+    const searchTerm: string = (
+      form.elements.namedItem('searchTerm') as HTMLInputElement
+    ).value.trim();
     evt.preventDefault();
     if (searchTerm.length < 3) {
       showToast();
